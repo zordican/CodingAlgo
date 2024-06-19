d1d712004f205b374f37e3510baa76bc5f511fb0
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-06-19 19:29:36 +0530

    .

---
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int l = 0;
+        int r = k;
+        int flag = false;
+        
+         map<int, int> mpp;
+        
+        for (int i = l; i <= r; i++) { //storing value in map
+            mpp[nums[i]]++;
+        }
+
+       
+        while (r < nums.size()) {
+            flag = false;
+            for (int i = l; i <= r; i++) {
+                cout<<mpp[nums[i]]<<" ";
+                if (mpp[nums[i]] > 1) {
+                    flag = true;
+                }
+            }
+            if(flag == true)
+                return true;
+
+            mpp[nums[l]]--;
+            l++;
+            
+            if(r<(nums.size()-1))r++;
+            mpp[nums[r]]++;
+            cout<<endl<<"Right wala ko badhane ke baad: "<<mpp[nums[r]];
+            cout<<endl;
+        }
+
+        return false;
+    }
+
+int main()
+{
+    vector<int> nums = {1,2,3,1,2,3};
+    containsNearbyDuplicate(nums,2);
+}
