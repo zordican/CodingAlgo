747dfaa0ae01a06f1467803a6fbdb883781b43ff
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-06-24 19:14:44 +0530

    GRAPH

---
+++ rough/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {};
+    for(auto it:v){
+        cout<<"I am running"<<endl;
+        cout<<it<<endl;
+    }
+}
