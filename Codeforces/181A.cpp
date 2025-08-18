cd604b58ab5330021845e918c27737f09945bacc
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2025-08-18 19:56:36 +0530

    test

---
+++ Codeforces/181A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+
+int32_t main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+   
+    while (t--) {
+       string s;
+       cin>>s;
+       sort(s.begin(),s.end(),greater<int>());
+        cout<<s<<endl;
+    }
+    return 0;
+}
+
