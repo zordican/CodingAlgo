343d647d028c228434099abfd19de2f6e32f9450
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2025-09-05 23:15:50 +0530

    gn

--- Codeforces/el.cpp
+++ Codeforces/el.cpp
@@ -11,25 +11,31 @@ int32_t main() {
 
     int t = 1;
     cin >> t;
-    long long mod = 1e9 + 7;
+    long long  = 1e9 + 7;
     while (t--) {
         long long n;
         cin >> n;
 
-        long long a = 1;
-        long long b = 1;
+        // long long a = 1;
+        // long long b = 1;
 
-        long long currMult = 1;
-        long long currSum = 1;
-        for(long long i = 1; i < n; i ++){
-            b++;
-            currMult = (b * a) % mod;             
-            currSum = (currSum + currMult) % mod;
-            a++;
-            currMult = (b * a) % mod;                
-            currSum = (currSum + currMult) % mod;
-        }
-        cout << (currSum * 2022)%mod << "\n";
+        // long long currMult = 1;
+        // long long currSum = 1;
+        // for(long long i = 1; i < n; i ++){
+        //     b++;
+        //     currMult = (b * a) % mod;             
+        //     currSum = (currSum + currMult) % mod;
+        //     a++;
+        //     currMult = (b * a) % mod;                
+        //     currSum = (currSum + currMult) % mod;
+        // }
+        long long ans = 337 % MOD;
+        ans = (ans * (n % MOD)) % MOD;
+        ans = (ans * ((n + 1) % MOD)) % MOD;
+        ans = (ans * ((4 * n - 1) % MOD)) % MOD;
+
+        cout << ans << "\n";
     }
     return 0;
 } 
+//337⋅𝑛(𝑛+1)(4𝑛−1)(mod109+7)
---
+++ Codeforces/raspberries.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+
+int32_t main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+   
+    while (t--) {
+        int n, k;
+
+        cin >> n >> k;
+        vector<int> v(n);
+
+        for(auto & it : v){
+            cin >> it;
+        }
+        if(k != 4){int mini = INT_MAX;
+
+        for(auto it : v){
+            mini = min(mini,it%k);
+        }
+        cout << mini << "\n";}
+        else{
+            int numEven = 0;
+            bool canbe = false;
+            for(auto it : v){
+                if(it%2 == 0){numEven++;if(numEven == 2){canbe = true; break;}}
+                if(it%4 == 0){
+                    canbe = true;
+                    break;
+                }
+            }
+            (canbe ? cout << 0 << "\n" : cout << 2 - numEven << "\n");
+        }
+    }
+    return 0;
+} 
